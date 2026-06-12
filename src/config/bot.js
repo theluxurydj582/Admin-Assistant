c81ea4c9ea24a21c38d913357c201b292ce5043f
theluxurydj582 <theluxurydj0@gmail.com>
2026-06-12 09:38:35 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Made By KJ Carlton",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 2, 
       },
     ],
   },
@@ -88,7 +88,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
+      primary: "#008000", 
       secondary: "#2F3136", 
 
       // Standard status colors for success/error/warning/info messages.
